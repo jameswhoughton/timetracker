018bdb3c01afdba134c9adc5d802c68f253ea73e
James Houghton <Jamesthornlea92@gmail.com>
2020-04-06 21:19:16 +0100

    Removed rounding from Time component

--- src/components/Time.vue
+++ src/components/Time.vue
@@ -9,29 +9,12 @@ export default {
     time: {
       type: Number,
       required: true,
-    },
-
-    round: {
-      type: Boolean,
-      required: false,
-      default: false,
     }
   },
 
   computed: {
     formattedTime() {
-      var time = this.time
-      if(this.round) {
-        time = this.roundTime(time)
-      }
-
-      return `${Math.floor(time / 60).toString().padStart(2, 0)}:${Math.abs(time % 60).toString().padStart(2, 0)}`
-    }
-  },
-
-  methods: {
-    roundTime(time) {
-      return 15 * Math.round(time / 15);
+      return `${Math.floor(this.time / 60).toString().padStart(2, 0)}:${Math.abs(this.time % 60).toString().padStart(2, 0)}`
     }
   }
 }
