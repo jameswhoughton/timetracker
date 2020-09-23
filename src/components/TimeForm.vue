56652b9ec64e43b0ea1af5ce707c071998bd0e30
James Houghton <jamesthornlea92@gmail.com>
2020-09-23 22:31:09 +0100

    Refinement

--- src/components/TimeForm.vue
+++ src/components/TimeForm.vue
@@ -6,21 +6,15 @@
     <td></td>
     <td class="buttons">
         <button 
-          v-show="!this.start" 
-          class="btn btn-success" 
-          @click="setNowTime('start')"
-        ><font-awesome-icon icon="play" /></button>
-        <button 
-          v-show="this.start && !this.end" 
+          v-show="start && !end" 
           class="btn btn-danger" 
           @click="stop()"
         ><font-awesome-icon icon="stop" /></button>
         <button 
-          v-show="this.start && this.end" 
-          class="btn btn-warning" 
-          @click="reset()"
-        ><font-awesome-icon icon="undo" /></button>
-        <button class="btn btn-success ml-1" @click="addTime()"><font-awesome-icon icon="plus" /></button>
+          v-show="isValid" 
+          class="btn btn-success" 
+          @click="addTime()"
+        ><font-awesome-icon icon="plus" /></button>
     </td>
   </tr>
 </template>
@@ -40,12 +34,16 @@ export default {
   computed: {
     started() {
       return this.start.length > 0
+    },
+
+    isValid() {
+      return this.description && this.start && this.end
     }
   },
 
   methods: {
     addTime() {
-      if(!this.valid()) {
+      if(!this.isValid) {
         alert('Description, start and end time required.')
         return
       }
@@ -65,14 +63,6 @@ export default {
       this.description = ""
     },
 
-    valid() {
-      if(!this.description || !this.start || !this.end) {
-        return 0;
-      }
-
-      return 1;
-    },
-
     setNowTime(type) {
       const time = new Date()
       const hours = time.getHours().toString().padStart(2, "0")
@@ -84,8 +74,16 @@ export default {
     stop() {
       this.setNowTime('end')
       
-      if(this.valid()) {
+      if(this.isValid) {
         this.addTime()
+      }
+    }
+  },
+
+  watch: {
+    description(val) {
+      if(val && !this.start) {
+        this.setNowTime('start')
       }
     }
   }
