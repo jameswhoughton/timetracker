1c92f30972d11821031bbc31fc4efecd84ad4846
James Houghton <jamesthornlea92@gmail.com>
2021-05-16 20:50:12 +0100

    Switch to yarn, update to Vue 3

--- src/main.js
+++ src/main.js
@@ -1,25 +1,18 @@
-import Vue from 'vue'
+import { createApp, h } from 'vue';
 import store from './store'
-import Vuex from 'vuex'
 
 import App from './App.vue'
-
-Vue.config.productionTip = false
-
-Vue.use(Vuex)
-//Vue.use(BootstrapVue)
-//Vue.use(IconsPlugin)
 
 store.subscribe((mutation, state) => {
   window.localStorage.setItem('times', JSON.stringify(state.times));
 })
 
-new Vue({
-  render: h => h(App),
-  store,
+createApp({
+  render: () => h(App),
+
   beforeCreate() {
 		const times = JSON.parse(window.localStorage.getItem('times')) || [];
 
     this.$store.commit('setTimes', times)
-	},
-}).$mount('#app')
+	}
+}).use(store).mount('#app')
